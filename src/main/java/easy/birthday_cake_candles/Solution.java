17035dd4603664812ecc89f7589f7922c3196a94
guluming <guluming@naver.com>
2022-09-20 23:21:58 +0900

    풀이 끝

---
+++ src/main/java/easy/birthday_cake_candles/Solution.java
@@ -0,0 +1,51 @@
+package easy.birthday_cake_candles;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        int answer = 0;
+
+        Collections.sort(candles, Collections.reverseOrder());
+        int max = candles.get(0);
+
+        for (int i = 0; i<candles.size(); i++) {
+            if (max == candles.get(i)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] candlesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> candles = new ArrayList<>();
+
+        for (int i = 0; i < candlesCount; i++) {
+            int candlesItem = Integer.parseInt(candlesTemp[i]);
+            candles.add(candlesItem);
+        }
+
+        int result = birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
